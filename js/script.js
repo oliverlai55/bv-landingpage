18c8acfa5871da8ccd97ce4166131d607b4b9fbf
Oliver Lai <oliverlai55@gmail.com>
2016-12-27 11:10:51 -0500

    Init commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
 
   (function initPercentage() {
-    var $progressbarWrap = $('.progressbar-wrapper').width()
-    var $progressBar = $('#progressbar')
-    var $percentText = $('#percent-text')
+    var $progressbarWrap = $('.progressbar-wrapper').width();
+    var $progressBar = $('#progressbar');
+    var $percentText = $('#percent-text');
     var setPercentage = setInterval(addPercentageText, 10);
     var $progressbarContent = $('.progressbar-content');
     var $searchbarContent = $('.searchbar-content');
 
     function addPercentageText() {
-        var barWidth = $progressBar.width()
-        var widthPercent = (100 - Math.round((barWidth / $progressbarWrap) * 100)) + '%'
+        var barWidth = $progressBar.width();
+        var widthPercent = (100 - Math.round((barWidth / $progressbarWrap) * 100)) + '%';
 
-        $percentText.text(widthPercent)
+        $percentText.text(widthPercent);
 
         if (barWidth == $progressbarWrap) {
           clearInterval(setPercentage);
-          
+
           $progressbarContent.addClass('hidden');
-          $searchbarContent.fadeIn(1500).removeClass('hidden');
+          $searchbarContent.fadeIn().removeClass('hidden');
         }
     }
   }());
